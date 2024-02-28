8b32e3b4d813697c2a3ee8d0b517ab92bbf5509d
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-02-28 17:54:27 -0500

    imported a lot of important stuff

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const gameBoard = document.getElementById("game-board");
+    
+
+    const colorExist = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "white", "maroon", "purple", "pink", "brown", "gray", "cyan", "magenta", "teal", "gold", "silver" ]
+
+    let colorPresent = [ ...colorExist, ...colorExist]
+
+    console.log(colorPresent)
+
+    function randomizer(originalArray){
+        let array = [...originalArray];
+        let shuffledArray = [];
+        while (array.length > 0){
+            const randomNumber = [Math.floor(Math.random() * array.length)];
+
+            shuffledArray.push(array[randomNumber]);
+            array.splice(randomNumber , 1);
+        }
+        return shuffledArray;
+    }
+
+    function game(){
+        cards = randomizer(colorPresent)
+        console.log(cards)
+    }
+
+
+
+    game()
+    });
+
+
+
+
+//createElement()
