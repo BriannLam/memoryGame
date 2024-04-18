7b6fe883b025f16de81142d5ac3f4c400b9b8c73
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-04-18 17:35:05 -0400

    made the board smaller and made cards selectable

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const gameBoard = document.getElementById("game-board");
     
 
-    const colorExist = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "white", "maroon", "purple", "pink", "brown", "gray", "cyan", "magenta", "teal", "gold", "silver" ]
+    const colorExist = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "white", "maroon", "purple", "pink", "brown", ]
 
     let colorPresent = [ ...colorExist, ...colorExist]
 
@@ -25,16 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     
-    function game(chosen){
+
+
+    function cardCreate(){
         let colors = randomizer(colorPresent)
+        let cardStack = []
+        let chosenCards = []
         for(let i=0; i < colors.length; i++){
             const card = document.createElement('div')
             card.classList.add('card');
+            card.id = "card"
             card.style.backgroundColor= "#000000";
             
 
             const frontFace = document.createElement('div')
-            frontFace.classList.add('face', 'front', );
+            frontFace.classList.add('face', 'front');
             frontFace.dataset.color = 'black'
 
             const backFace = document.createElement('div')
@@ -52,26 +57,59 @@ document.addEventListener("DOMContentLoaded", () => {
             
             console.log(card)
             gameBoard.appendChild(card)
-            return card;
+
+
+            card.addEventListener('click', function() {
+                console.log('Card clicked:', card);
+                card.style.backgroundColor = "none"
+                limit(card); // Call the check function whenever a card is clicked
+            });
+    
+            cardStack.push(card);
         }
         
-
-
-        card.addEventListener("click",wtf())
-        console.log(chosen)
-    }
-
-    function wtf(){
-        console.log('reach me')
-    }
-
-    function check(selected,card){
-        console.log("reach me")
-        if(selected.length<2){
-            selected.push(card)
-            flipped(card)
+       function limit(clickedCard){
+        if (chosenCards.length < 2){
+            chosenCards.push(clickedCard); // Push the clicked card into the chosenCards array
         }
+        if (chosenCards.length == 2){
+            flipped()
+            check()
+            stopClick()
             
+        }
+        console.log(chosenCards);
+    } 
+
+    function stopClick(){
+        cardStack.forEach(function(card) {
+                card.removeEventListener('click', function() {
+                console.log('Card clicked:', card);
+                card.style.backgroundColor = "none"
+                limit(card); // Call the check function whenever a card is clicked
+            });
+            });
+    }
+
+
+    function check(){
+        
+    }
+    function flipped(){
+        
+    }
+    function clickHandler(){
+        console.log('limit reached')
+    }
+    }
+
+    
+
+
+
+    function game(){
+        cardCreate()
+    }
 
         // 1st: Make if statement to check that there isn't already two cards flipped
         // 2nd: if two cards don't have flipped class then add flipped class to selected cards 
@@ -79,7 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
         // 4th else: if less than two cards are flipped then add flipped class and save to new array 
         // if not run check function to check of two cards in array are the same color 
         // if not the same color 
-    }
 
 
 function flipped(card){
@@ -102,7 +139,7 @@ function check(){
 
 
 
-    game(chosen)
+    game()
     });
 
 
