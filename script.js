05c3b019872dd19f00e031e03cee8c89197a2c91
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-04-25 17:42:35 -0400

    finishing the color version of game

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// const e = require("express")
+
 let chosenCards = []
 let finishCards = []
 
 let currColor = ''
-let afterColor = ''
+let firstColor = ''
 
 let count = 0
 let first_instance = -1 
 
+var canClick = true;
+
 document.addEventListener("DOMContentLoaded", () => {
     const gameBoard = document.getElementById("game-board");
     const colorExist = [
+        // "url(FILE_PATH)"
         "red", 
         "orange", 
         "yellow", 
@@ -57,63 +62,96 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(colorCardStack)
 
         for (let i = 0; i < colorCardStack.length; i++) {
-                colorCardStack[i].addEventListener("click", function() {
-                    changeColor(i)
-                });
+            colorCardStack[i].addEventListener("click", function() {
+                if (canClick) {
+                    // Execute the click action only if a click is allowed
+                    changeColor(i);
+                    canClick = false; // Disable further clicks temporarily
+                    setTimeout(function() {
+                        canClick = true;
+                    }, 600);
+                }
+            });
         }
         
         function winCondition(index){
-            if(finishCards.length == 1 ){
+            if(finishCards.length == 2 ){
                 colorCardStack[index].style.backgroundColor = chosenCards[index]
                 alert('You won')
+                setTimeout(function(){ 
+                    location.reload();
+                 }, 500);
             }
         }
         
 
         function changeColor(index) {
+            if(colorCardStack[first_instance] === colorCardStack[index]){
+                colorCardStack[first_instance].style.backgroundColor === colorCardStack[index].style.backgroundColor
+            }
+
             if(count == 0){ // Count = 0, WHICH IS ----> 
                 colorCardStack[index].style.backgroundColor = chosenCards[index]
+                colorCardStack[index].classList.add('flip');
                 currColor = chosenCards[index] // TELLS the computer what the next card should be 
                 first_instance = index; // Logs the index of the first card in the pair
+                chosenCards[index] = "#000000"
                 console.log(currColor)
             }
             if(count == 1){ // This checks if the card youre clicking on is the second card in the pair
                 if(index != first_instance){
                     colorCardStack[index].style.backgroundColor = chosenCards[index]
-                    afterColor = chosenCards[index] // Another version of currColor except that it is for the second card being clicked
-                    console.log(afterColor)
+                    colorCardStack[index].classList.add('flip');
+                    firstColor = chosenCards[index] // Another version of currColor except that it is for the second card being clicked
+                    console.log(firstColor)
                 }
+                setTimeout(() => {
+                    colorCardStack[first_instance].classList.remove('flip');
+                    colorCardStack[index].classList.remove('flip');
+                }, 500); // Adjust the delay as needed
+                
             }
 
             count++
 
             // CHECK LOGIC
-            if(count == 2 && currColor !== afterColor){ //If they do not match they wull go back to the color black
+            if(count == 2 && currColor !== firstColor ){ //If they do not match they wull go back to the color black
+
+                chosenCards[first_instance] = currColor
+                // let first_instance_color  = colorCardStack[first_instance].style.backgroundColor;
+                // chosenCards.push(first_instance_color, chosenCards[first_instance])
 
                 // Set timeout for reveal, before going black
 
                 // SET TIMEOUT FUNCION BEFORE RUNNING THIS    
+
                 setTimeout(function(){ 
+                    console.log(count)
+                    console.log(colorCardStack[first_instance])
+                    console.log(chosenCards)
                     colorCardStack[first_instance].style.backgroundColor = "#000000"  
                     colorCardStack[index].style.backgroundColor = "#000000"
+                    
                  }, 500);
-      
-                console.log("TEST")
                 count = 0
             }
-            if(count == 2 && currColor === afterColor){ //If they match then it resets the count and instance and should do win thing
+            if(count == 2 && currColor === firstColor){ //If they match then it resets the count and instance and should do win thing
                 if(index != first_instance){
-                    console.log("TEST")
+                    console.log("match")
                     finishCards.push(chosenCards[index])
                     console.log(finishCards)
                     setTimeout(function(){ 
                         winCondition(index)
                      }, 500);
+                     currColor = ''
+                     firstColor = ''
+                     console.log(currColor)
+                     console.log(firstColor)
                     first_instance = -1; // Basically tells the computer that youre on a new color
-                    count = 1
-                    //win thingy
+                    count = 0
                 }
-                count = 1
+                
+                
             }
         }
 
@@ -127,3 +165,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // BUGS TO FIX //
     // 1. Clicking the SAME card and it still resets count // HARDEST BUG
+    // window.location.reload()
